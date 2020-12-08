cf395e8191234cdead32da66e1972f772c39e1ad
高义翔 <gaoyixiang@gaoyixiangdeMacBook-Pro.local>
2020-12-08 19:00:42 +0800

    修改了footer对齐

--- dist/js/banner.js
+++ dist/js/banner.js
@@ -1,23 +1,21 @@
 var scrollFunc=function(e){
-    console.log(e.wheelDelta);
-    // ee=e || window.event;
-    // var t1=document.getElementById("wheelDelta");
-    // var t2=document.getElementById("detail");
-    // if(e.wheelDelta){//IE/Opera/Chrome
-    //     t1.value=e.wheelDelta;
-    // }else if(e.detail){//Firefox
-    //     t2.value=e.detail;
-    // }
-    console.log()
-    if(e.wheelDelta<0&&$('.container').css('top')!=0){
-        $('.video-wrapper').removeClass('hide1')
-        $('.container').removeClass('hide')
-        console.log('成功')
+    // console.log(e.wheelDelta);
+    if(e.wheelDelta<0){
+        console.log(1)
+        $('#box1').css({"top":"-100%"})
+        $('#box2').css({"top":"0","position":"static"})
+        $('.front-mask').removeClass('hide').addClass('show')
+    }
+    else if(e.wheelDelta>0&&$('#box2').offset().top - $(window).scrollTop()==0){
+        $('#box1').css("top","0")
+        $('#box2').css({"top":"100%","position":"fixed"})
+        $('.front-mask').removeClass('show').addClass('hide')
+
     }
 }
 /*注册事件*/
 if(document.addEventListener){
     document.addEventListener('DOMMouseScroll',scrollFunc,false);
-}//W3C
+}
 window.onmousewheel=document.onmousewheel=scrollFunc;
 
